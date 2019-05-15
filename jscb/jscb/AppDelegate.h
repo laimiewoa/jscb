216f96c50dfe1337b79c886b39a2a8606e43f0a5
laimiewoa <fycdr@qq.com>
2019-05-15 18:55:15 +0800

    初始化项目，金山词霸-每日一句

---
+++ jscb/jscb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jscb
+//
+//  Created by 台电 on 2019/5/15.
+//  Copyright © 2019 台电. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jscb/jscb/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jscb
+//
+//  Created by 台电 on 2019/5/15.
+//  Copyright © 2019 台电. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
